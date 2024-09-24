650edbbb34cc22816b64e88ceed4fbcb52e34d91
SandipKurmi <sandipkurmi0@gmail.com>
2024-09-24 09:43:24 +0530

    Implement basic JavaScript challenges
    - Hello World: Return "Hello World!" string
    - Get Sum: Add two numbers and return the result
    - Calculator: Perform basic arithmetic operations (+, -, *, /)
    - Count Occurrences: Count character occurrences in a string
    
    Remove corresponding test files for each challenge.

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,17 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  const strArray = str.split("");
+
+  let charCount = 0;
+
+  for (let index = 0; index < strArray.length; index++) {
+    const element = strArray[index];
+
+    if (element === char) {
+      charCount = charCount + 1;
+    }
+  }
+
+  return charCount;
+}
 
 module.exports = countOccurrences;
